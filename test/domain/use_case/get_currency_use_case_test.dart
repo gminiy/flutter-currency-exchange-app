849b313a5d4ddf13b819131d1813cbf614588365
gminiy <gmini.y@gmail.com>
2024-01-09 19:02:27 +0900

    feat: get_currency_use_case_test.dart 미완성, 바톤터치

---
+++ test/domain/use_case/get_currency_use_case_test.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_exchange_currency/core/result.dart';
+import 'package:flutter_exchange_currency/data/data_source/remote/conversion_rate_api.dart';
+import 'package:flutter_exchange_currency/data/repository/conversion_rate_repository_impl.dart';
+import 'package:flutter_exchange_currency/domain/use_case/get_currency_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('getCurrencyUseCase test', () async {
+    final ConversionRateApi api = ConversionRateApi(dio: Dio());
+    final repository = ConversionRateRepositoryImpl(api: api);
+    final GetCurrencyUseCase getCurrencyUseCase = GetCurrencyUseCase(
+        baseCode: 'USD',
+        targetCode: 'KOR',
+        conversionRateRepository: repository);
+    final Result<num> currency = await getCurrencyUseCase.execute();
+    print(currency);
+  });
+}
