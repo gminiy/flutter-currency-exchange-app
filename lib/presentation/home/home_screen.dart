d503cc9a47c734d7ccb5df7719a817d1e5921645
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-09 20:25:03 +0900

    feat: DropDownMenu 추가

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exchange_currency/core/constant/currency_codes.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,36 +12,92 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            onChanged: (value) {},
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2,
-                  color: Colors.purpleAccent,
-                ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.purpleAccent,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.cyanAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    hint: Text('Choose a code'),
+                    onChanged: (baseCode) {
+                      print(baseCode);
+                    },
+                    items: currencyCodes.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          TextField(
-            onChanged: (value) {},
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2,
-                  color: Colors.purpleAccent,
-                ),
+              const SizedBox(
+                height: 24,
               ),
-            ),
-          )
-        ],
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.purpleAccent,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.cyanAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    hint: Text('Choose a code'),
+                    onChanged: (targetCode) {
+                      print(targetCode);
+                    },
+                    items: currencyCodes.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
