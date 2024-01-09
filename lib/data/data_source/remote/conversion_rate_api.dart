cf891eb758c9fdb6028f8e0ae24f16ab7110e27a
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-09 17:24:38 +0900

    feat: Conversion Rate API call

---
+++ lib/data/data_source/remote/conversion_rate_api.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class ConversionRateApi {
+  final String _baseUrl = 'https://v6.exchangerate-api.com';
+
+  final Dio _dio;
+
+  const ConversionRateApi({
+    required Dio dio,
+  }) : _dio = dio;
+
+  Future<Map<String, dynamic>> getConversionRate(
+      {String baseCode = 'USD'}) async {
+    final String uri = '$_baseUrl/v6/acfe21c07bf14765b0ce1f17/latest/$baseCode';
+    final response = await _dio.get(uri);
+    return response.data as Map<String, dynamic>;
+  }
+}
